bfe23571028cf3a5e7e874cd007ac4263a22b3bf
Irene <irene.ghioni@gmail.com>
2026-06-17 17:14:10 +0200

    initialized and completed main.py - parsing_thread.py and resilience.py adapted to not crush under main.py

--- src/resilience.py
+++ src/resilience.py
@@ -1,15 +1,3 @@
-# CHECKPOINT MANAGING
-# Saves the index of the last results page (e.g., "page_index": 45) to a local file (checkpoint.json).
-# If the script crashes, it tells the crawler which index to restart from.
-
-# GARANTEE UNIQUENESS
-# It must keep track of all the ad URLs already collected.
-# A function instantly answers True or False to the question: "Is this URL new or have we already saved it?".
-
-# PERSISTENCE ON DATASET BUILDING
-# While the Parser extracts structured data about a house (price, rooms, sqm, etc.),
-# this module takes that dictionary and immediately append it to the final dataset file (immovlan_dataset.csv or .jsonl) on real time.
-
 import json
 from pathlib import Path
 import threading
@@ -18,9 +6,9 @@ import threading
 class StateManager:
 
     def __init__(self, 
-                 csv_path: str = "fetched_urls.csv", 
-                 json_path: str = "checkpoint.json",
-                 dataset_path: str = "properties.jsonl"):
+                 csv_path: Path | str, 
+                 json_path: Path | str,
+                 dataset_path: Path | str):
         # creation of Path objs
         self.json_file = Path(json_path)
         self.csv_file = Path(csv_path)
@@ -32,13 +20,15 @@ class StateManager:
         # check if file already exists on the hard disk
         # If json file already exists, it means that it crashed in the middle of last session
         if self.json_file.exists():
-            with open(self.json_file, "r") as url_bookmark:
+            with self.json_file.open("r") as url_bookmark:
+            # with open(self.json_file, "r") as url_bookmark:
                 completed_list = json.load(url_bookmark)
                 self.completed_indices = set(completed_list)  # set of all completed indices
         else:
             # create json file from scratch
             self.completed_indices = set()
-            with open(self.json_file, "w") as url_bookmark:
+            with self.json_file.open("w") as url_bookmark:
+            # with open(self.json_file, "w") as url_bookmark:
                 json.dump(list(self.completed_indices), url_bookmark)
 
         # make sure that the csv file exists on the hard disk
@@ -49,10 +39,12 @@ class StateManager:
             # dict_data argument is a dictionary containing all the cleaned data extracted by the parser
             data["property_url"] = url
             # save the dict in a jsonl file
-            with open(self.jsonl_properties_dataset, "a") as properties_dataset:
+            with self.jsonl_properties_dataset.open("a") as properties_dataset:
+            # with open(self.jsonl_properties_dataset, "a") as properties_dataset:
                 property_info = json.dumps(data)
                 properties_dataset.write(property_info + "\n")
-            with open(self.csv_file, "a") as urls_log:
+            with self.csv_file.open("a") as urls_log:
+            # with open(self.csv_file, "a") as urls_log:
                 urls_log.write(url + "\n")
 
     def save_url_checkpoint(self, saved_index):
@@ -61,7 +53,8 @@ class StateManager:
 
             # atomic writing to secure the file content
             temp_json_file = self.json_file.with_name(self.json_file.name + ".tmp")
-            with open(temp_json_file, "w") as temp_bookmark:
+            with temp_json_file.open("w") as temp_bookmark:
+            # with open(temp_json_file, "w") as temp_bookmark:
                 json.dump(list(self.completed_indices), temp_bookmark)
             temp_json_file.replace(self.json_file)
 
